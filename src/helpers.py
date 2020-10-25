3d01d16fd58592e357f75b775959d0256f41b6cc
Kryštof Kiss <krystof.kiss@backbone.sk>
2020-10-25 09:30:27 +0100

    create mips_decoder do custom decode json

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,23 @@
 import json
 
 
+def mips_decoder(x):
+    if isinstance(x, dict):
+        new_dict = {}
+        for k, v in x.items():
+            try:
+                new_dict[int(k)] = v
+            except ValueError:
+                new_dict[k] = v
+
+                if isinstance(v, str) and v.startswith('0b'):
+                    new_dict[k] = int(v, 2)
+
+        return new_dict
+
+    return x
+
+
 def load_file(path: str):
     with open(file=path) as f:
         return f.read().splitlines()
@@ -8,4 +25,4 @@ def load_file(path: str):
 
 def load_json(path: str):
     with open(file=path) as json_file:
-        return json.load(json_file)
+        return json.load(json_file, object_hook=mips_decoder)
